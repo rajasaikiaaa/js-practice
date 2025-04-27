41b59f067ef2a35e62765e73fbb32367675f1720
Raja <rajasaikiaaa@gmail.com>
2025-04-27 10:37:36 +0530

    Add while loop to a function

--- whileloop.js
+++ whileloop.js
@@ -1,5 +1,18 @@
-let x = 1;
-while ( x <= 30){
+/*let x = 1;
+while ( x <= 10){
     console.log(x);
     x++;
-}
+}*/
+
+const num = () => {
+    let x = ;
+    while( x <= 5){
+    if ( x = 1){
+        console.log(x);
+    }
+    x++;
+
+    }
+};
+num();
+console.log(num());
