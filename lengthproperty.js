de87b764f1850ac0685589f3219b46118cbdb2bd
Raja <rajasaikiaaa@gmail.com>
2025-04-28 13:48:55 +0530

    learn length property of array and string

---
+++ lengthproperty.js
@@ -0,0 +1,8 @@
+const array = ['Banana','Pineapple','Jackfruit','Watermelon','Apple','Avocado'];
+console.log(array);
+
+console.log(array.length); //counts the length of the array.
+
+
+const string = "Dolor enim sint fugiat consequat ex dolore magna.";
+console.log(string.length); //counts the number of letter of the string.
