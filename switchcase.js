bb06e5b5a9658454df865816ed876f14344f64a1
Raja <rajasaikiaaa@gmail.com>
2025-04-29 13:47:53 +0530

    Learn switch case

---
+++ switchcase.js
@@ -0,0 +1,25 @@
+let ludos = Math.floor(Math.random() * 7);
+switch (ludos){
+    case 0:
+        console.log('Your Ludo number is 6');
+        break;
+    case 1:
+        console.log('Your ludo number is ' + ludos);
+         break;
+    case 2:
+        console.log('Your ludo number is ' + ludos);
+         break;
+    case 3:
+        console.log('Your ludo number is ' + ludos);
+        break;
+    case 4:
+        console.log('Your ludo number is ' + ludos);
+        break;
+    case 5:
+        console.log('Your ludo number is ' + ludos);
+        break;
+    case 6:
+        console.log('Your ludo number is ' + ludos);
+        break;
+    default:
+}
