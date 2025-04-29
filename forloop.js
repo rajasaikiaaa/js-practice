f9ffd20e6ce8258d5a6cdf8a06fd69355ca3941d
Raja <rajasaikiaaa@gmail.com>
2025-04-29 13:46:12 +0530

    Learn for loop

---
+++ forloop.js
@@ -0,0 +1,19 @@
+//creating a program that gives output from 1 to 10 using for loop
+for (let num = 1; num <= 10; num++){  //for (initialization; condition; updation)
+    console.log(num);
+}
+
+// loop in reverse that prints from 10 to 0
+
+for (let count = 10; count >= 0; count--){
+    console.log(count);
+}
+
+//loops through arrays
+
+const array = ['Banana','Pineapple','Jackfruit','Watermelon','Apple','Avocado'];
+console.log(array);
+ 
+for ( let i = 0; i < array.length; i++){
+    console.log(array[i]);
+}
