9a0fc2c0a683797c64407add10042af00741e047
Raja <rajasaikiaaa@gmail.com>
2025-04-30 12:50:14 +0530

    learn break statement

---
+++ break.js
@@ -0,0 +1,20 @@
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+
+for (let i = 0; i < array.length; i++) {
+    if (i > 15) {
+        break;
+    }
+    console.log("Yeah Boi");
+    
+} 
+console.log("End of Array");
+
+
+
+
+for (let i = 0; i <= 99; i++){
+    if (i > 20){
+        break;
+    }
+    console.log(`ai ${i}`);
+} console.log("Ar parlamna ami")
