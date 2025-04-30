5272180d2dfa5df4400e04e896d54efc8419916a
Raja <rajasaikiaaa@gmail.com>
2025-04-30 12:50:41 +0530

    learn do while loop

---
+++ dowhileloop.js
@@ -0,0 +1,23 @@
+// do while loop executes at least once without checking the condition , followed by the updation and then condition.
+
+const num = 10;
+let numAdd = 0;
+
+do {
+    console.log(numAdd)
+    numAdd++;
+} while (numAdd <= num){
+    console.log('End')
+};
+
+
+
+const num1 = 10;
+let numAdd1 = 0;
+
+do {
+    numAdd1++;
+    console.log(numAdd1)
+} while (numAdd1 <= num1){
+    console.log('End')
+};
