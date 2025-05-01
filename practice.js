85009d58e9cecaf9b23a687305f4a11962491d13
Raja <rajasaikiaaa@gmail.com>
2025-05-01 13:50:46 +0530

    passing a function in an array

---
+++ practice.js
@@ -0,0 +1,14 @@
+const array = ['aloo', 'mutton', 'dahi'];
+const array2 = [];
+
+function pushItem (arr){
+    arr.push('Royal Enfield');
+}
+
+
+console.log(array);
+pushItem(array);
+console.log(array);
+
+pushItem(array2);
+console.log(array2);
